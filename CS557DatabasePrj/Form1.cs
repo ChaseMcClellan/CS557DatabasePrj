9bdba22cacccfdb491473019a14da57f4b3edb7b
Chase McClellan <mcclel54@uwm.edu>
2025-09-30 00:03:43 -0500

    added some psudocode logic

--- CS557DatabasePrj/Form1.cs
+++ CS557DatabasePrj/Form1.cs
@@ -14,12 +14,21 @@ namespace CS557DatabasePrj
 
         private void frmLogin_Load(object sender, EventArgs e)
         {
-
+            //init DB
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-
+            //psudocode when we implement
+            /*
+             * if(findandreturnUser(txtUsername.text,txtPassword.text) != null){
+                    if(user.role == 1){open Userform;}
+                    else if (user.role == 2) {open Tellerform;}
+                    else if (user.role == 3) {open adminForm;}
+            }else{lblAlert.hidden == true} //invalid user if method returns null then show lblAlert saying wrong credentials 
+               }
+             
+            */
         }
     }
 }
